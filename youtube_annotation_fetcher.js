c5c43ebbb27dc9039a05e43f1a0e2a05a14651f2
Eric <eric.kumlin@gmail.com>
2017-04-09 14:47:26 -0700

    Update to include Cards as well

--- youtube_annotation_fetcher.js
+++ youtube_annotation_fetcher.js
@@ -17,31 +17,44 @@
 		$("link[rel=icon]").attr("href", "https://s.ytimg.com/yts/img/favicon-vfldLzJxy.ico");
 
 		$("#creator-page").on("click", ".edit-expand-menu-button", function() {
-			var $annotation = $(this).find(".vm-video-annotations").html("<i>Loading Annotations...</i>");
-			var annotationLink = $annotation.attr("href");
+			var $annotationsButton = $(this).find(".vm-video-annotations").html("<i>Loading visual elements...</i>");
+			var $cardsButton = $(this).find(".vm-video-cards").html("<i>Loading Cards...</i>");
+
+			var annotationLink = $annotationsButton.attr("href");
 			var videoId = annotationLink.substr(annotationLink.indexOf("v=") + 2);
-			var annotationsUrl = window.location.origin + "/annotations_auth/read2?video_id=" + videoId;
+
+			var endScreenUrl = window.location.origin + "/endscreen_ajax?v=" + videoId + "&encrypted_video_id=" + videoId + "&action_load=1";
+			var cardsUrl = window.location.origin + "/cards_ajax?v=" + videoId + "&action_list=1";
 
 			$.ajax({
-				url: annotationsUrl,
+				url: endScreenUrl,
 				dataType: "text",
 				success: function(data) {
-					var count = (data.match(/openUrl/g) || []).length;
-					$annotation.text("Annotations (" + count + ")");
+					var count = (data.match(/"callToAction"/g) || []).length;
+					$annotationsButton.text("End Screen Links (" + count + ")");
 				},
 				statusCode: {
 					403: function() {
-						var endScreenUrl = window.location.origin + "/endscreen_ajax?v=" + videoId + "&encrypted_video_id=" + videoId + "&action_load=1";
+						var annotationsUrl = window.location.origin + "/annotations_auth/read2?video_id=" + videoId;
 
 						$.ajax({
-							url: endScreenUrl,
+							url: annotationsUrl,
 							dataType: "text",
 							success: function(data) {
-								var count = (data.match(/"callToAction"/g) || []).length;
-								$annotation.text("End Screen Links (" + count + ")");
+								var count = (data.match(/openUrl/g) || []).length;
+								$annotationsButton.text("Annotations (" + count + ")");
 							},
 						});
 					},
+				},
+			});
+
+			$.ajax({
+				url: cardsUrl,
+				dataType: "text",
+				success: function(data) {
+					var count = (data.match(/"target_url"/g) || []).length;
+					$cardsButton.text("Cards (" + count + ")");
 				},
 			});
 		});
